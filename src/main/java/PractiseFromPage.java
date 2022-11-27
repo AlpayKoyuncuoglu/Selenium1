b9c81d9ad1d0bc02acc76515bd5111576b1b6bb5
AlpayKoyuncuoglu <alpaykoyuncuoglu1@gmail.com>
2022-11-27 22:04:18 +0300

    first

---
+++ src/main/java/PractiseFromPage.java
@@ -0,0 +1,17 @@
+public class PractiseFromPage    {
+
+    public void setName(){
+
+    }
+
+    public void setLastName(){
+
+    }
+
+    public void email(){
+
+    }
+
+
+
+}
---
+++ src/test/java/PersonelInformationTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+public class PersonelInformationTest
+{
+    @Test
+    public void test_setName()
+    {
+        System.out.println("test m1");
+    }
+
+    @Test
+    public void test_setLastName()
+    {
+        System.out.println("test m2");
+    }
+
+
+}
